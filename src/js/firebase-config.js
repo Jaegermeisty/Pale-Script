26dbbeec1fb33603b4276db01abc37fb5fc94f86
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-06-27 21:40:34 +0200

    Added firebase config js file with starter code

---
+++ src/js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgrfbUa2011oIufOkuw2YdMYVJEWUtnk4",
+  authDomain: "palescript.firebaseapp.com",
+  projectId: "palescript",
+  storageBucket: "palescript.firebasestorage.app",
+  messagingSenderId: "570950982879",
+  appId: "1:570950982879:web:c8f3e01c7552edf1095d6a",
+  measurementId: "G-G3WZGT2W8X"
+};
